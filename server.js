5ea6609fbf391f5d01e5d39ec6192221dbf3d719
Glenn Spinner <glenngineer1@gmail.com>
2016-09-27 13:43:27 -0500

    changed server.js to use static path of public/index.html

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express()
 const port = process.env.PORT || 3000
 app.set('port', port)
 
+// middlewares
+app.use(express.static('public'))
+
 app.get('/', (req, res) =>
   res.send('Welcome To My Mini Auth App!')
 )
